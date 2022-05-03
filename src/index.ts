ca6a5dfa4d34ec2d6e339e467ca81e272642b2f9
invalidcode232 <invalidcode69@gmail.com>
2022-05-03 21:49:58 +0700

    init

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+// Initialize environment variables
+require('dotenv').config();
+
+// Imports
+const { Client, Intents } = require('discord.js');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.login(process.env.TOKEN);
+
